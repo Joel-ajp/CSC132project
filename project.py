dbc7ce21487aa56908f03ac6d24db41b114a5e18
Joel Porter <andrewjoelporter@gmail.com>
2023-03-31 23:58:43 -0500

    Slight Changes

--- project.py
+++ project.py
@@ -4,18 +4,19 @@
 # Description: CSC 132 Final Project
 ######################################################################################################################
 
-# The code to get and display the soil moisture and temperature data.
 
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
+# Imports the needed libraries
+import tkinter as tk
 import time
-
 import board
-
 from adafruit_seesaw.seesaw import Seesaw
 
+
+
+# The code to get and display the soil moisture and temperature data.
+
 i2c_bus = board.I2C()  # uses board.SCL and board.SDA
+
 # i2c_bus = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
 ss = Seesaw(i2c_bus, addr=0x36)
@@ -27,10 +28,22 @@ while True:
     # read temperature from the temperature sensor
     temp = ss.get_temp()
 
+    # Prints the data 
     print("temp: " + str(temp) + "  moisture: " + str(touch))
     time.sleep(1)
 
 
+# Beginning of the GUI
+mainWindow = Tk()
+
+
+
+
+
+
+
 
 ###MAIN###
+
+
 
