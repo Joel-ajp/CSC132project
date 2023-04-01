cb1335f18df92d7c80ebc9e298bb2f112b44bed2
Joel Porter <andrewjoelporter@gmail.com>
2023-03-31 23:54:22 -0500

    Added the soil and tenp getter

---
+++ plantProject.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import abc
+import os
+import gpio
+
+class PlantData(metaclass = abc.ABCMeta):
+    """A class to house all of the methods to pull plant input data"""
+    def __init__(self):
+        pass
+    @abc.abstractclassmethod
+    def 
+
+    
+            
+        
+            
+    
+
+
