81df1cf58f122886e4de2a9f4dc7b829d2783ab3
Joel Porter <andrewjoelporter@gmail.com>
2023-04-11 23:28:55 -0500

    12

--- webapp.py
+++ webapp.py
@@ -4,32 +4,12 @@
 # Description: CSC 132 Final Project
 ######################################################################################################################
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import json
-from requests import get
 import time
-#import board
+import board
 from adafruit_seesaw.seesaw import Seesaw
 from multiprocessing import Process
-
-# The code to get and display the soil moisture and temperature data.
-
-#i2c_bus = board.I2C()  # uses board.SCL and board.SDA
-
-# i2c_bus = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-
-#ss = Seesaw(i2c_bus, addr=0x36)
-
-# while True:
-#     # read moisture level through capacitive touch pad
-#     touch = ss.moisture_read()
-
-#     # read temperature from the temperature sensor
-#     temp = ss.get_temp()
-
-#     # Prints the data 
-#     print("temp: " + str(temp) + "  moisture: " + str(touch))
-#     time.sleep(1)
 
 # Helper function to easly  parallelize multiple functions
 def parallelize_functions(*functions):
@@ -58,8 +38,29 @@ def landingPageIndex():
 def temperaturePageIndex():
     return render_template("temp.html")
 
+# Moisture page function
+@app.route("/moisture")
+def moisturePageIndex():
+    return render_template("moisture.html")
+
+# Light page function
+@app.route("/light")
+def lightPageIndex():
+    return render_template("light.html")
+
+
+
+
+# The code to get and display the soil moisture and temperature data.
+
+i2c_bus = board.I2C()  # uses board.SCL and board.SDA
+
+# i2c_bus = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+
+ss = Seesaw(i2c_bus, addr=0x36)
+
 # A function that gets the temperature of the plant perpetually
-def while_function():
+def getMoisTemp():
     touch = ss.moisture_read()
 
     # read temperature from the temperature sensor
@@ -69,11 +70,13 @@ def while_function():
     print("temp: " + str(temp) + "  moisture: " + str(touch))
     time.sleep(1)
 
+
+
 # A function to run the app
 def run_app():
     app.run(debug=False)
 
-
+# To run the app
 if __name__ == "__main__":
-    # parallelize_functions(while_function, run_app)
+    # parallelize_functions(getMoisTemp, run_app)
     app.run(host=HOST,port=PORT,debug=DEBUG)
